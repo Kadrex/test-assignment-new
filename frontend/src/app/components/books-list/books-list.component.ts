5a712bdf4d02911c8ea7a6a84b3cd5c3d98b80f4
Kadri Männimets <kamann@ttu.ee>
2020-11-09 17:57:07 +0200

    Support paging and sorting for books table.

--- frontend/src/app/components/books-list/books-list.component.ts
+++ frontend/src/app/components/books-list/books-list.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { BookService } from '../../services/book.service';
-import { Observable } from 'rxjs';
-import { Page } from '../../models/page';
+import {Page} from '../../models/page';
 import { Book } from '../../models/book';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
   styleUrls: ['./books-list.component.scss']
 })
-export class BooksListComponent implements OnInit {
+export class BooksListComponent implements OnInit, AfterViewInit {
 
-  books$: Observable<Page<Book> | Error>;
+  pageSizeOptions: number[] = [10, 15, 20, 50];
+  displayedColumns: string[] = ['title', 'author', 'genre', 'available'];
+  dataSource: MatTableDataSource<Book> = new MatTableDataSource<Book>();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
   private yes: string = 'Yes';
   private no: string = 'No';
+
 
   constructor(
     private bookService: BookService,
@@ -21,9 +28,16 @@ export class BooksListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // TODO this observable should emit books taking into consideration pagination, sorting and filtering options.
-    this.books$ = this.bookService.getBooks({});
+    this.bookService.getBooks({pageIndex: 0, pageSize: 5000})
+      .subscribe(res => {
+        let x = res as Page<Book>;
+        this.dataSource.data = x.content
+      })
+  }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   isAvailable(book): string {
