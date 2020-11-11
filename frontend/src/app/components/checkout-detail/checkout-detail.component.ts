6cc5b89504cf128b8cd3f904af493ae53e5e716f
Kadri Männimets <kamann@ttu.ee>
2020-11-11 23:31:20 +0200

    Implement returning books.

--- frontend/src/app/components/checkout-detail/checkout-detail.component.ts
+++ frontend/src/app/components/checkout-detail/checkout-detail.component.ts
@@ -4,6 +4,10 @@ import { ActivatedRoute } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
 import {CheckoutService} from "../../services/checkout.service";
 import {Checkout} from "../../models/checkout";
+import {BookService} from "../../services/book.service";
+import {BookStatus} from "../../models/book-status";
+import {ConfirmationDialog} from "../confirmation-dialog/confirmation-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-checkout-detail',
@@ -11,12 +15,16 @@ import {Checkout} from "../../models/checkout";
   styleUrls: ['./checkout-detail.component.scss']
 })
 export class CheckoutDetailComponent implements OnInit {
-  checkout$: Observable<Checkout | Error>;
 
+  private BOOK_STATUS_AVAILABLE: BookStatus = 'AVAILABLE';
+  private dialogText: string = 'Do you really wish to return this book?';
+  checkout$: Observable<Checkout | Error>;
 
   constructor(
     private route: ActivatedRoute,
     private checkoutService: CheckoutService,
+    private bookService: BookService,
+    private dialog: MatDialog
   ) {
   }
 
@@ -24,6 +32,25 @@ export class CheckoutDetailComponent implements OnInit {
     this.checkout$ = this.route.params
       .pipe(map(params => params.id))
       .pipe(switchMap(id => this.checkoutService.getCheckout(id)))
+  }
+
+  openDialog(id, book): void {
+    const dialogRef = this.dialog.open(ConfirmationDialog, {
+      data: {text: this.dialogText
+      }});
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res) {
+        this.returnBook(id, book)
+      }
+    })
+  }
+
+  returnBook(id, book): void {
+    book.status = this.BOOK_STATUS_AVAILABLE;
+    this.bookService.saveBook(book.id).subscribe();
+    this.checkoutService.deleteCheckout(id).subscribe();
+    window.location.href = '/checkouts';
   }
 
 }
